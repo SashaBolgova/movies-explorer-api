37a8857f9253673da41cdbb30ecf929a36360e6d
Sasha Bolgova <aleks.kostyleva@yandex.ru>
2023-07-06 19:25:35 +0300

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,29 +1,25 @@
 require('dotenv').config();
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { JWT_SECRET = 'some-secret-key' } = process.env;
 
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-err');
 const ERROR_401_MESSAGE = require('../utils/constants');
 
 const auth = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError(ERROR_401_MESSAGE));
-  }
-
-  const token = authorization.replace('Bearer ', '');
-  let payload;
-
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+      throw new UnauthorizedError(ERROR_401_MESSAGE);
+    }
+    const token = authorization.replace('Bearer ', '');
+
+    req.user = jwt.verify(token, JWT_SECRET);
+    next();
   } catch (err) {
-    return next(new UnauthorizedError(ERROR_401_MESSAGE));
+    next(new UnauthorizedError(ERROR_401_MESSAGE));
   }
-  req.user = payload;
-  next();
-  return null;
 };
 
 module.exports = { auth };
