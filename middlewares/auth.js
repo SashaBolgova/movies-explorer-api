3f17a066606b48cb61388694ab356bb27c0e1965
Sasha Bolgova <aleks.kostyleva@yandex.ru>
2023-07-07 00:58:17 +0300

    token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,19 +7,18 @@ const UnauthorizedError = require('../errors/unauthorized-err');
 const ERROR_401_MESSAGE = require('../utils/constants');
 
 const auth = (req, res, next) => {
-  try {
-    const { authorization } = req.headers;
 
-    if (!authorization || !authorization.startsWith('Bearer ')) {
-      throw new UnauthorizedError(ERROR_401_MESSAGE);
-    }
-    const token = authorization.replace('Bearer ', '');
+  const { token } = req.cookies;
 
-    req.user = jwt.verify(token, JWT_SECRET);
-    next();
-  } catch (err) {
-    next(new UnauthorizedError(ERROR_401_MESSAGE));
+  if (!token) {
+    throw new UnauthorizedError(ERROR_401_MESSAGE);
   }
-};
+
+
+  req.user = jwt.verify(token, JWT_SECRET);
+  next();
+} catch (err) {
+  next(new UnauthorizedError(ERROR_401_MESSAGE));
+}
 
 module.exports = { auth };
